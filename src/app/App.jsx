b56b134eee107f218fe943aad7396010790bf04a
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-10-24 19:47:45 -0500

    Axios new structure
    Axios new structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,13 +4,15 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
+  const IPLOCAL = import.meta.env.VITE_IPV4;
+  const PORT = import.meta.env.VITE_SERVERPORT;
 
   const [videos, setVideos] = useState([]);
   useEffect(() => {
     const fetchVideos = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/api/v1/video');
-        setVideos(response.data);
+        const response = await axios.get(`${IPLOCAL}:${PORT}/api/v1/video`);
+        setVideos(response.data.videos || []);
       } catch (error) {
         console.error('Error al obtener los videos:', error);
       }
@@ -23,13 +25,18 @@ function App() {
   // http://localhost:3000/uploads/videoplayback.mp4
   return (
     <>
-      <h1>App</h1>
+      <h1>App mis curso</h1>
       <div>
         {videos.length > 0 ? (
           videos.map((video) => (
             <div key={video.id} className="video-card">
               <h3>{video.title}</h3>
-              <video src={`http://localhost:3000${video.path}`} width="500" height="auto" controls="controls">
+              <video
+                src={`${IPLOCAL}:${PORT}${video.path}`}
+                width="500"
+                height="auto"
+                controls
+              >
                 Tu navegador no soporta la reproducción de video.
               </video>
             </div>
@@ -38,7 +45,6 @@ function App() {
           <p>No hay videos disponibles</p>
         )}
       </div>
-
     </>
   )
 }
